f4366a13c8315a168a0ebd5b889b8ec49fbb3893
miguelcoria94 <miguelcoria94@gmail.com>
2021-01-06 16:38:09 -0600

    fix 404 header

--- react-app/src/components/static_pages/NoMatch.js
+++ react-app/src/components/static_pages/NoMatch.js
@@ -6,22 +6,15 @@ import spaceman from "../images/spaceman.png"
 const LogoutButton = ({ setAuthenticated }) => {
     return (
       <Jumbotron fluid className="bg-404">
-            <img
-                alt="spaceman"
+        <img
+          alt="spaceman"
           width={500}
           src={spaceman}
           height={500}
           className="animate__animated animate__slideInRight"
         />
-        <h1 className="title-404">This Page is Lost in Space</h1>
-        <p className="text-404">
-          You thought this mission to the moon would be a quick six month thing.
-          Your neighbor offered to look after your dog. Your high school math
-          teacher was impressed. He once said you wouldn’t amount to
-          anything.You sure showed him. But now here you are, fifty feet from
-          your spaceship with no way to get back. Your dog will be so sad. Your
-          math teacher will be so smug. Pretty devastating.
-        </p>
+        <h1 className="title-404">"Houston, we have a problem"</h1>
+        <a href="/">Go back home</a>
       </Jumbotron>
     );
 };
