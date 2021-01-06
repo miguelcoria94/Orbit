94fb9e5c5c58df216f7585a2d909a9a03e17c12e
miguelcoria94 <miguelcoria94@gmail.com>
2021-01-05 22:36:44 -0600

    fix spaceman size

--- react-app/src/components/static_pages/NoMatch.js
+++ react-app/src/components/static_pages/NoMatch.js
@@ -1,9 +1,23 @@
 import React from "react";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import "bootstrap/dist/css/bootstrap.min.css";
+import spaceman from "../images/spaceman.png"
 
 const LogoutButton = ({ setAuthenticated }) => {
-  return <image src=""/>;
+    return (
+      <Jumbotron>
+            <img width={500} src={spaceman} height={500}/>
+        <h1>This Page is Lost in Space</h1>
+        <p>
+          You thought this mission to the moon would be a quick six month thing.
+          Your neighbor offered to look after your dog. Your high school math
+          teacher was impressed. He once said you wouldn’t amount to
+          anything.You sure showed him. But now here you are, fifty feet from
+          your spaceship with no way to get back. Your dog will be so sad. Your
+          math teacher will be so smug. Pretty devastating.
+        </p>
+      </Jumbotron>
+    );
 };
 
 export default LogoutButton;
