cff1517d330fb3c3c278b8344bdb491733ac1f01
miguelcoria94 <miguelcoria94@gmail.com>
2021-01-05 23:07:04 -0600

    fix the nav bar

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,36 +1,25 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav"
 
 const NavBar = ({ setAuthenticated }) => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to="/" exact={true} activeClassName="active">
+    <Navbar>
+          <Nav.Link href="/" exact={true} activeClassName="active">
             Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/login" exact={true} activeClassName="active">
+          </Nav.Link>
+          <Nav.Link href="/login" exact={true} activeClassName="active">
             Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/sign-up" exact={true} activeClassName="active">
+          </Nav.Link>
+          <Nav.Link to="/sign-up" exact={true} activeClassName="active">
             Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/users" exact={true} activeClassName="active">
+          </Nav.Link>
+          <Nav.Link to="/users" exact={true} activeClassName="active">
             Users
-          </NavLink>
-        </li>
-        <li>
+          </Nav.Link>
           <LogoutButton setAuthenticated={setAuthenticated} />
-        </li>
-      </ul>
-    </nav>
+    </Navbar>
   );
 }
 
