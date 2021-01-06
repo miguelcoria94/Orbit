ca74db943f10737d83bafd954c1022bf743bba48
miguelcoria94 <miguelcoria94@gmail.com>
2021-01-05 22:27:37 -0600

    working on 404

---
+++ react-app/src/components/static_pages/NoMatch.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const LogoutButton = ({ setAuthenticated }) => {
+  return <image src=""/>;
+};
+
+export default LogoutButton;
