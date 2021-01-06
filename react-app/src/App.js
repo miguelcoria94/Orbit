e8218b04975b97784e7485c03bf2bea653532e1e
miguelcoria94 <miguelcoria94@gmail.com>
2021-01-05 22:17:49 -0600

    created an about page

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,6 +7,7 @@ import ProtectedRoute from "./components/auth/ProtectedRoute";
 import UsersList from "./components/UsersList";
 import User from "./components/User";
 import { authenticate } from "./services/auth";
+import About from "./components/static_pages/About"
 
 function App() {
   const [authenticated, setAuthenticated] = useState(false);
@@ -49,7 +50,7 @@ function App() {
           <h1>My Home Page</h1>
         </ProtectedRoute>
         <Route exact path="/about">
-          
+          <About />
         </Route>
       </Switch>
     </BrowserRouter>
