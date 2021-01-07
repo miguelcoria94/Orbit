143f7c57841bebfd7cd118f35f792519aab80607
miguelcoria94 <miguelcoria94@gmail.com>
2021-01-06 20:47:20 -0600

    fix header

--- react-app/src/components/static_pages/Home.js
+++ react-app/src/components/static_pages/Home.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import { Row, Col, Container } from "react-bootstrap/";
+import control from "../images/control.png"
 
 const Home = ({ setAuthenticated }) => {
 
-    return <Container>
+    return (
+      <Container>
         <Row>
-            <Col className="main-header-container">
-                <h1 className="main-header-h1">Mission control but for your money</h1>
-            </Col>
-      </Row>
-  </Container>;
+          <Col className="main-header-container">
+            <h1 className="main-header-h1">
+              Mission <span>control</span>, but for your money.
+            </h1>
+          </Col>
+          <Col className="main-header-container">
+                    <img src={control}/>
+          </Col>
+          
+        </Row>
+      </Container>
+    );
 };
 
 export default Home;
