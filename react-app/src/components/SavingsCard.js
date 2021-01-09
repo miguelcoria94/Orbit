1f6b6f5619670d87aed083c7863740c25bd67e88
miguelcoria94 <miguelcoria94@gmail.com>
2021-01-09 01:25:04 -0600

    got balance working

--- react-app/src/components/SavingsCard.js
+++ react-app/src/components/SavingsCard.js
@@ -1,20 +1,21 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-function SavingsCard() {
+function SavingsCard({ currentUserId }) {
   const [balance, setBalance] = useState([]);
 
   useEffect(() => {
-    (async () => {
-      const data = await axios.get(
-        `http://127.0.0.1:5000/api/savings_account/${user_id}`
+		(async () => {
+			const data = await axios.get(
+        `http://127.0.0.1:5000/api/savings_account/${currentUserId}`
       );
-      setBalance(data);
-    })();
-    console.log("hi");
-  }, [balance]);
-
-  return <h1>test</h1>;
+			setBalance(data.data.savings_balance[0]);
+		})();
+  }, []);
+  
+  return (
+    <div>{balance.balance ? (<h1>${balance.balance}</h1>) : (<h1>{"$0"}</h1>)}</div>
+  );
 }
 
 export default SavingsCard;
