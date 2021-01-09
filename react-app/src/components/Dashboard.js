3e6f587d9b96f3c98ed0ba43a5bf3e07cd8f77ba
miguelcoria94 <miguelcoria94@gmail.com>
2021-01-08 19:08:49 -0600

    add css for dashboard

--- react-app/src/components/Dashboard.js
+++ react-app/src/components/Dashboard.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Redirect } from "react-router-dom";
 import { Row, Col, Container } from "react-bootstrap/";
-import SideNav from "./SideNav"
-
+import SideNav from "./SideNav";
+import "Dashboard.css"
 
 const Dashboard = () => {
     return (
-      <Container>
+      <Container className="dashboard-wrapper">
             <SideNav />
       </Container>
     );
