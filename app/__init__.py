9d83e0a8d954c2bc2417ea728f5ca8686a8534f1
miguelcoria94 <miguelcoria94@gmail.com>
2021-01-08 23:18:52 -0600

    I can get a balnce now

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ from flask_login import LoginManager
 from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
-from .api.savings_account_route import savings_account_route
+from .api.savings_account_routes import savings_account_routes
 
 
 from .seeds import seed_commands
