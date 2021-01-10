82be125715cd6020f12fa16c61a7e019283ed41c
miguelcoria94 <miguelcoria94@gmail.com>
2021-01-09 20:00:29 -0600

    add quick pay styles

--- react-app/src/components/QuickPay.js
+++ react-app/src/components/QuickPay.js
@@ -1,13 +1,59 @@
 import React, { useEffect, useState } from "react";
+import { Redirect } from "react-router-dom";
+import { Container , Row} from "react-bootstrap/";
+import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 import "./Dashboard.css";
 
 const QuickPay = () => {
-    return (
-        <div className="Card custom-card">
+    const [errors, setErrors] = useState([]);
+    const [userEmail, setUserEmail] = useState("");
+    const [senderId, setSenderId] = useState("");
+    const [amount, setAmount] = useState("");
 
-        </div>
-    )
+    const updateEmail = (e) => {
+        setUserEmail(e.target.value);
+    };
+
+    const updateAmount = (e) => {
+        setAmount(e.target.value);
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    }
+    
+    return (
+      <div className="Card custom-card last-card">
+        <p className="active-text">Quick Pay</p>
+        <form onSubmit={handleSubmit} className="small-form">
+          <div>
+            {errors.map((error) => (
+              <div>{error}</div>
+            ))}
+            <div className="small-input-wrapper">
+              <label>Email</label>
+              <input
+                name="text"
+                type="text"
+                placeholder="$100"
+                onChange={updateAmount}
+              />
+            </div>
+            <div className="small-input-wrapper">
+              <label>Password</label>
+              <input
+                name="password"
+                type="password"
+                placeholder=""
+                onChange={updateEmail}
+              />
+              <button type="submit">Send Now</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    );
 }
 
 export default QuickPay;
