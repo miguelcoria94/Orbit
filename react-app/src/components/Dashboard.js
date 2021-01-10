fa0b927f5847830b79ce0a8600cd376c483187d1
miguelcoria94 <miguelcoria94@gmail.com>
2021-01-09 22:24:44 -0600

    change chart color

--- react-app/src/components/Dashboard.js
+++ react-app/src/components/Dashboard.js
@@ -6,6 +6,9 @@ import LogoutButton from "./auth/LogoutButton"
 import SavingsCard from "./SavingsCard"
 import CheckingsCard from "./CheckingsCard";
 import QuickPay from "./QuickPay";
+import { Chart } from "react-charts";
+import { LineChart, PieChart } from "react-chartkick";
+import "chart.js";
 
 const Dashboard = ({ setAuthenticated, currentUser, currentUserId }) => {
   return (
@@ -19,12 +22,20 @@ const Dashboard = ({ setAuthenticated, currentUser, currentUserId }) => {
         </Col>
         <Col className="col-3">
           <CheckingsCard currentUserId={currentUserId} />
+          <div className="chart-div">
+            <LineChart
+              colors={["rgb(50, 173, 81)", "#666"]}
+              label="Value"
+              data={{ 1: 3500, 2: 4000, 3: 3600, 4: 5000 }}
+            />
+          </div>
         </Col>
         <Col col-3>
           <SavingsCard currentUserId={currentUserId} />
         </Col>
         <Col col-3 className="last-div">
           <QuickPay currentUserId={currentUserId} />
+          <PieChart data={{ Blueberry: 1, Strawberry: 3 }} />
         </Col>
       </Row>
     </Container>
