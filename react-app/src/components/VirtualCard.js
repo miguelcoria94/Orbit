7a2b1296a6029326526485450b4c80477a1986e3
miguelcoria94 <miguelcoria94@gmail.com>
2021-01-13 17:32:08 -0600

    fix the indention

--- react-app/src/components/VirtualCard.js
+++ react-app/src/components/VirtualCard.js
@@ -4,15 +4,15 @@ import SideNav from "./SideNav";
 
 const VirtualCard = ({ currentUserId, currentUser, setAuthenticated }) => {
     return <Container fluid className="dashboard-wrapper">
-      <Row className="transfer-wrapper">
-        <Col className="col-3">
-          <SideNav
-            title={"Transfers"}
-            currentUser={currentUser}
-            setAuthenticated={setAuthenticated}
-          />
-            </Col>
-            </Row>
+                <Row className="transfer-wrapper">
+                    <Col className="col-3">
+                    <SideNav
+                        title={"Transfers"}
+                        currentUser={currentUser}
+                        setAuthenticated={setAuthenticated}
+                    />
+                    </Col>
+                </Row>
             </Container>
 }
 
