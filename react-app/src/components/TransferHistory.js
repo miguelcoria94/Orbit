5acd3b8feafd2d93079e3d4a3395944fa4a02042
miguelcoria94 <miguelcoria94@gmail.com>
2021-01-13 13:43:15 -0600

    get the transfer history on page

--- react-app/src/components/TransferHistory.js
+++ react-app/src/components/TransferHistory.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Row, Col, Container } from "react-bootstrap/";
+import axios from "axios";
 
 const TransferHistory = ({currentUserId}) => {
+    const [userTransfers, setUserTransfers] = useState([]);
 
+    useEffect(() => {
+      (async () => {
+        const data = await axios.get(`/api/users/${currentUserId}/transfer-history`);
+        setUserTransfers(data.data.users);
+      })();
+    }, []);
+
+    console.log(userTransfers)
 
     return <Container>
         <Row>
             <Col className="col-12"><p>Transfer History</p></Col>
-
         </Row>
+            {userTransfers.map((transfer, id) => (
+                <Row>
+                    <Col className="ol-3">
+                        <p>{transfer.amount}</p>
+                    </Col>
+                </Row>
+            ))}
     </Container>
 }
 
