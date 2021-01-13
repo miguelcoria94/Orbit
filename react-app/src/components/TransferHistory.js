bc3e72f7950cd0f8c8877fa87e8a5587b26c66b5
miguelcoria94 <miguelcoria94@gmail.com>
2021-01-13 13:33:52 -0600

    set up user id in component

--- react-app/src/components/TransferHistory.js
+++ react-app/src/components/TransferHistory.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Row, Col, Container } from "react-bootstrap/";
 
-const TransferHistory = () => {
-    return <h1>Hello</h1>
+const TransferHistory = ({currentUserId}) => {
+
+
+    return <Container>
+        <Row>
+            <Col className="col-12"><p>Transfer History</p></Col>
+
+        </Row>
+    </Container>
 }
 
 export default TransferHistory;
