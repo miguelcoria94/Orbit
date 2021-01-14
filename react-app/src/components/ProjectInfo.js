daa5e96b042d8986afc28dfde3b9427ecd6cb7d8
miguelcoria94 <miguelcoria94@gmail.com>
2021-01-14 09:25:24 -0600

    adjust project info

---
+++ react-app/src/components/ProjectInfo.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import { Row, Col, Container } from "react-bootstrap/";
+
+const ProjectInfo = () => {
+    return (
+        <Container>
+
+        </Container>
+    )
+}
+
+export default ProjectInfo
