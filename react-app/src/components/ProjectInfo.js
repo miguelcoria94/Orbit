f109cfd90062b3a85215ad1d33adbc09e893fb11
miguelcoria94 <miguelcoria94@gmail.com>
2021-01-17 03:30:37 -0600

    add subheading

--- react-app/src/components/ProjectInfo.js
+++ react-app/src/components/ProjectInfo.js
@@ -5,7 +5,17 @@ import "./ProjectInfo.css"
 const ProjectInfo = () => {
     return (
         <Container className="aboutPageContainer">
-            <h1 className="about-title">Welcome to Orbit !</h1>
+            <h1 className="about-title">About Orbit</h1>
+            <Row>
+                <Col>
+                    <h3 className="about-subheading">How Orbit works.</h3>
+                    <p>
+
+                    </p>
+                </Col>
+                <Col>
+                </Col>
+            </Row>
         </Container>
     )
 }
