47ca2c0d32b68b5f68ba0ab1883195582c5c3c09
miguelcoria94 <miguelcoria94@gmail.com>
2021-01-17 03:41:45 -0600

    change the about feature text

--- react-app/src/components/ProjectInfo.js
+++ react-app/src/components/ProjectInfo.js
@@ -9,9 +9,15 @@ const ProjectInfo = () => {
             <Row>
                 <Col>
                     <h3 className="about-subheading">How Orbit works.</h3>
-                    <p>
-
+                    <p className="about-feature">Active your accounts.
                     </p>
+                    <p className="about-feature-text">Load money into your account and beginning divvy money up between your checkings and savings account. </p>
+                    <p className="about-feature">Active your accounts.
+                    </p>
+                    <p className="about-feature-text">Load money into your account and beginning divvy money up between your checkings and savings account. </p>
+                    <p className="about-feature">Active your accounts.
+                    </p>
+                    <p className="about-feature-text">Load money into your account and beginning divvy money up between your checkings and savings account. </p>
                 </Col>
                 <Col>
                 </Col>
