33c023439cf521a556f4dea10e9e9c5a3ffb8460
miguelcoria94 <miguelcoria94@gmail.com>
2021-01-17 03:53:34 -0600

    add feature list

--- react-app/src/components/ProjectInfo.js
+++ react-app/src/components/ProjectInfo.js
@@ -11,13 +11,16 @@ const ProjectInfo = () => {
                     <h3 className="about-subheading">How Orbit works.</h3>
                     <p className="about-feature">Active your accounts.
                     </p>
-                    <p className="about-feature-text">Load money into your account and beginning divvy money up between your checkings and savings account. </p>
-                    <p className="about-feature">Active your accounts.
+                    <p className="about-feature-text">Load money into your account and beginning divvying up your money between your checkings and savings account. </p>
+                    <p className="about-feature">Send money to friends or family.
                     </p>
-                    <p className="about-feature-text">Load money into your account and beginning divvy money up between your checkings and savings account. </p>
-                    <p className="about-feature">Active your accounts.
+                    <p className="about-feature-text">Easily send money to friends or family with just there email.</p>
+                    <p className="about-feature">Create Virtual Cards.
                     </p>
-                    <p className="about-feature-text">Load money into your account and beginning divvy money up between your checkings and savings account. </p>
+                    <p className="about-feature-text">Virtual Cards provides a solution that allows you to generate unique card numbers for free for your online purchases.</p>
+                    <p className="about-feature">Create goals and stay motivated
+                    </p>
+                    <p className="about-feature-text">You can use Goals to set money aside while keeping it handy when you need it. Deleting a goal adds the balance right back to your account.</p>
                 </Col>
                 <Col>
                 </Col>
