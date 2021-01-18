8e93bc120acfc0be19ff5b14ac206de728574b31
miguelcoria94 <miguelcoria94@gmail.com>
2021-01-18 08:27:50 -0600

    fix sign up errors

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -12,6 +12,7 @@ const SignUpForm = ({authenticated, setAuthenticated}) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [repeatPassword, setRepeatPassword] = useState("");
+  const [errors, setErrors] = useState("")
 
   const onSignUp = async (e) => {
     e.preventDefault();
@@ -20,6 +21,8 @@ const SignUpForm = ({authenticated, setAuthenticated}) => {
       if (!user.errors) {
         window.location.reload();
         setAuthenticated(true);
+      } else {
+        setErrors("Problem Signing Up")
       }
     }
   };
@@ -125,8 +128,15 @@ const SignUpForm = ({authenticated, setAuthenticated}) => {
               ></input>
             </Col>
           </Row>
-          <button className="demo-button" type="submit">
-            Sign Up
+          <button
+            type="submit"
+            className={
+              errors
+                ? "activate-savings-button animate__animated animate__shakeX transfer-button demo-button"
+                : "add-funds-button transfer-button demo-button"
+            }
+          >
+            {errors ? `${errors}` : "Login"}
           </button>
         </form>
       </Row>
