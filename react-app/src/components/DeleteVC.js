621194f1cb1e9caa7728d1731e311278a39a9862
miguelcoria94 <miguelcoria94@gmail.com>
2021-01-18 04:44:09 -0600

    fix the transfer margin bug

---
+++ react-app/src/components/DeleteVC.js
@@ -0,0 +1,19 @@
+import React from "react";
+import axios from "axios";
+
+const AddFunds = ({ cardId, cardBalance }) => {
+    const deleteCard = async (e) => {
+        const request = { cardBalance, cardId };
+        const response = await axios.put(`/api/checkings_account/`, request);
+        window.location.reload()
+        return response
+    };
+
+    return (
+        <button className="add-funds-button add-funds-space" onClick={deleteCard}>
+            Delete Card
+        </button>
+    );
+};
+
+export default AddFunds;
--- react-app/src/components/VirtualCardHistory.js
+++ react-app/src/components/VirtualCardHistory.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Row, Col, Container } from "react-bootstrap/";
 import axios from "axios";
+import DeleteVC from "./DeleteVC"
 
 const VirtualCardHistory = ({ currentUserId }) => {
     const [virtualCards, setVirtualCard] = useState([]);
@@ -14,52 +15,63 @@ const VirtualCardHistory = ({ currentUserId }) => {
 
 
     return (
-        <Container className="virtual-card-transfer-container h-100">
+        <div className="virtual-card-transfer-container h-100">
             <Row>
                 <Col className="col-12">
                     <p className="transfer-table-title virtual-card-title">Virtual Card History</p>
                 </Col>
             </Row>
             <Row className="info-row">
-                <Col className="col-3 table-label-wrapper">
+                <Col className="col-2 table-label-wrapper">
                     <p className="table-label">
                         <i class="fas fa-store from-icon">
                         </i>Merchant
             </p>
                 </Col>
-                <Col className="col-3 table-label-wrapper">
+                <Col className="col-2 table-label-wrapper">
                     <p className="table-label">
                         <i class="far fa-money-bill-alt amount-icon"></i>Amount
             </p>
                 </Col>
-                <Col className="col-3 table-label-wrapper">
+                <Col className="col-2 table-label-wrapper">
                     <p className="table-label">
                         <i class="fas fa-question to-icon"></i>Status
             </p>
                 </Col>
-                <Col className="col-3 table-label-wrapper">
+                <Col className="col-2 table-label-wrapper">
                     <p className="table-label">
-                        <i class="far fa-calendar-alt table-icon"></i>Date
+                        <i class="far fa-calendar-alt table-icon"></i>Date Created
+            </p>
+                </Col>
+                <Col className="col-2 table-label-wrapper">
+                    <p className="table-label">
+                        <i class="fab fa-cc-visa to-icon"></i>Card Number
             </p>
                 </Col>
             </Row>
             {virtualCards.map((card, id) => (
                 <Row className="info-row">
-                    <Col className="col-3 info">
+                    <Col className=" info">
                         <p>{card.merchant}</p>
                     </Col>
-                    <Col className="col-3 info">
-                        <p>{card.amount}</p>
+                    <Col className=" info">
+                        <p>${card.amount}</p>
                     </Col>
-                    <Col className="col-3 info">
+                    <Col className=" info">
                         <p>{card.status}</p>
                     </Col>
-                    <Col className="col-3 info">
+                    <Col className=" info">
                         <p>{card.date.slice(4, 16)}</p>
+                    </Col>
+                    <Col className="info">
+                        <p>{card.card_number}</p>
+                    </Col>
+                    <Col className="info">
+                        <DeleteVC cardId={card.id} cardBalance={card.amount}/>
                     </Col>
                 </Row>
             ))}
-        </Container>
+        </div>
     );
 }
 
