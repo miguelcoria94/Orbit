c77890f64fb26287964d65a13981f75552f1e7c1
miguelcoria94 <miguelcoria94@gmail.com>
2021-01-18 00:58:18 -0600

    setup  virtual cards

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,3 +3,4 @@ from .user import User
 from .savings_account import Savings_Account
 from .checkings_account import Checkings_Account
 from .account_transfers import Account_Transfers
+from .virtual_cards import Virtual_Cards
