41d2b1735a586d47c316568017e5b93e1aeb22a3
miguelcoria94 <miguelcoria94@gmail.com>
2021-01-19 04:29:48 -0600

    setting up expense page

--- react-app/src/components/ExpenseTracking.js
+++ react-app/src/components/ExpenseTracking.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Row, Col, Container } from "react-bootstrap/";
 import SideNav from "./SideNav";
+import { PieChart } from "react-chartkick";
 
 
 const ExpenseTracking = ({ currentUserId, currentUser, setAuthenticated }) => {
@@ -16,6 +17,23 @@ const ExpenseTracking = ({ currentUserId, currentUser, setAuthenticated }) => {
                     />
                 </Col>
                 <Col >
+                    <Row>
+                        <Col>
+                            <h1 className="bug-report-title">Expense Breakdown</h1>
+                            <PieChart data={[["Blueberry", 44], ["Strawberry", 23]]} />
+                        </Col>
+                        <Col className="bug-report-wrapper">
+                            <h1 className="bug-report-title">Add Expense</h1>
+                            <form>
+                                <input name="firstname" className="input w-100" type="text" placeholder="First Name">
+                                </input>
+                                <input name="firstname" className="input w-100" type="text" placeholder="First Name">
+                                </input>
+                                <input name="firstname" className="input w-100" type="text" placeholder="First Name">
+                                </input>
+                            </form>
+                        </Col>
+                    </Row>
                 </Col>
             </Row>
         </Container>
